010feec52d9ebaebe77980d128c78fd7d8f18432
unknown <az09johnray@gmail.com>
2024-01-23 10:32:47 +0800

    New file

---
+++ demo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Simple Form Processing</title>
+</head>
+
+<body>
+    <form action="form_1.php" method="post">
+        FirstName:
+        <inpit type="text" name="firstname" required/>
+        <br>
+        <br>
+        LastName
+        <inpit type="text" name="lastname" required/>
+        <br>
+        <br>
+        Address
+        <input type="text" name="address" required/>
+        <br>
+        <br>
+        Email Address:
+        <input type="email" name="email" required/>
+        <br>
+        <br>
+        Password:
+        <input type="password" name="password" required/>
+        <br>
+        <br>
+        <input type="submit" value=Submit>
+</form>
+</body>
+</html>
+        
+
